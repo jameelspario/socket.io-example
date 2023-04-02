23541efbb2ae16553d519ab80e302d874a3420be
jameel <android2@theinteractive.in>
2023-04-02 17:59:57 +0530

    android client update

---
+++ socketioandroidchatclient/app/src/main/java/com/example/androidclient/Constants.java
@@ -0,0 +1,7 @@
+package com.example.androidclient;
+
+public class Constants {
+//    public static final String CHAT_SERVER_URL = "https://socket-io-chat.now.sh/";
+    public static final String CHAT_SERVER_URL = "http://192.168.0.11:3000";
+
+}
